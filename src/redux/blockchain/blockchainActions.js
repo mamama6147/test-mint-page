7e7aae013eec079d97522f038af1560992e4ed0d
Nashio6441 <nashio6441@gmail.com>
2023-11-11 13:50:38 +0900

    本番用コントラクトを設定し、ネットワークをメインネットに変更。

--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -101,8 +101,8 @@ export const connect = () => {
             // check if the chain to connect to is installed
             await window.ethereum.request({
               method: 'wallet_switchEthereumChain',
-              // params: [{ chainId: '0x1' }], // イーサチェーンのID（16進数）
-              params: [{ chainId: '0x5' }], // Goerli テストネットのID（16進数）
+              params: [{ chainId: '0x1' }], // イーサチェーンのID（16進数）
+              // params: [{ chainId: '0x5' }], // Goerli テストネットのID（16進数）
               // params: [{ chainId: '0x4' }], // rinkebyのID（16進数）
               // params: [{ chainId: '0x89' }], // polygonMainのID（16進数）
             });
@@ -114,8 +114,8 @@ export const connect = () => {
                   method: 'wallet_addEthereumChain',
                   params: [
                     {
-                      // chainId: '0x1', //イーサ
-                      chainId: '0x5', //Goerli
+                      chainId: '0x1', //イーサ
+                      // chainId: '0x5', //Goerli
                       // chainId: '0x89', //polygonMain
                       // rpcUrl: 'https://mainnet-infura.brave.com/',
                     },
